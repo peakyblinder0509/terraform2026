452c41660d6336bcb0dc6a8f63a871cf512205d8
peakyblinder0509 <connect2ikram509@gmail.com>
2026-06-23 09:45:15 +0530

    Add subnet_id to aws_instance resource

--- cf.tf
+++ cf.tf
@@ -1,8 +1,7 @@
 resource "aws_instance" "web" {
   ami           = "ami-01a00762f46d584a1"
   instance_type = "t3.micro"
-  
-
+  subnet_id = "subnet-0e8b198666f3e71e2"
   tags = {
     Name = "HelloWorld"
   }
